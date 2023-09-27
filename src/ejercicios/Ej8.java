fcdf78cf53b1eef78ee24a2cd7676a7666fd1c9c
enieves <enieves@106-02.info2>
2023-09-27 10:15:12 +0200

    put the ternary

--- src/ejercicios/Ej8.java
+++ src/ejercicios/Ej8.java
@@ -13,6 +13,8 @@ public class Ej8 {
 		adultos=20;
 		niños=15.50;
 		
+		//se aplica un ternario que dice qwue si el valor es >100, se aplica el descuento
+		
 		//cierre de scanner
 		sc.close();
 	}
