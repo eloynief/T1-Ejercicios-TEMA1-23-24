2a7e5e9227721d54acf4f5876f3f2f9612c39db3
enieves <enieves@106-02.info2>
2023-09-28 12:27:39 +0200

    test

--- src/ejercicios/Ej10.java
+++ src/ejercicios/Ej10.java
@@ -9,6 +9,22 @@ public class Ej10 {
 		//creación de scanner
 		Scanner sc=new Scanner(System.in);
 		
+		//
+		double m;
+		int cm;
+
+		//dice en la pantalla que escribamos un numero
+		System.out.print("Escribe un numero: ");
+		
+		//escritura del numero
+		m=sc.nextInt();
+		
+		//se hace una conversion de double a int para que lea el numero sin decimales
+		cm=(int)m*100;
+		
+		
+		
+		
 		//cierre de scanner
 		sc.close();
 	}
--- src/ejercicios/Ej9.java
+++ src/ejercicios/Ej9.java
@@ -8,6 +8,27 @@ public class Ej9 {
 		
 		//creación de scanner
 		Scanner sc=new Scanner(System.in);
+
+		//
+		int num,num2;
+		
+
+		//dice en la pantalla que escribamos un numero
+		System.out.print("Escribe un numero: ");
+		
+		//escritura del numero
+		num=sc.nextInt();
+		
+
+		// nos pide por pantalla que escribamos otro numero
+		System.out.print("Escribe otro numero: ");
+		
+		//escritura del numero
+		num2=sc.nextInt();
+		
+		//muestra false si los numeros no son iguales
+		System.out.println(num==num2);
+		
 		
 		//cierre de scanner
 		sc.close();
